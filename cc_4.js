b16822f88dac93cbdcf0c5bef3d91d97921b7d60
Collin Gourley <cgourley@usf.edu>
2025-09-18 13:53:02 -0400

    Finished Step 2

---
+++ cc_4.js
@@ -0,0 +1,9 @@
+    // Step 2 Create an array
+     let products = [
+    { name: "Skate Blades", category: "Skate", price: 39.99, inventory: 25 },
+    { name: "Helmet", category: "Headwear",    price: 79.99, inventory: 32 },
+    { name: "Stick",  category: "Equipment", price: 129.99,  inventory: 60 },
+    { name: "Knee Pads",  category: "Padding", price: 35.99, inventory: 8 },
+    { name: "Tape", category: "Miscellaneous", price: 5.99, inventory: 40 },
+    ];
+    //Step 3 Write a for...of loop
