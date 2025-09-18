400f6044b4e3903567b1db9f97cb531740a9ded5
Collin Gourley <cgourley@usf.edu>
2025-09-18 14:40:04 -0400

    End of step 3

--- cc_4.js
+++ cc_4.js
@@ -1,9 +1,24 @@
     // Step 2 Create an array
-     let products = [
-    { name: "Skate Blades", category: "Skate", price: 39.99, inventory: 25 },
-    { name: "Helmet", category: "Headwear",    price: 79.99, inventory: 32 },
-    { name: "Stick",  category: "Equipment", price: 129.99,  inventory: 60 },
-    { name: "Knee Pads",  category: "Padding", price: 35.99, inventory: 8 },
-    { name: "Tape", category: "Miscellaneous", price: 5.99, inventory: 40 },
-    ];
+      let products = [
+        { name: "Skate Blades", category: "Skate", price: 39.99, inventory: 25 },
+        { name: "Helmet", category: "Headwear",    price: 79.99, inventory: 32 },
+        { name: "Stick",  category: "Equipment", price: 129.99,  inventory: 60 },
+        { name: "Knee Pads",  category: "Padding", price: 35.99, inventory: 8 },
+        { name: "Tape", category: "Miscellaneous", price: 5.99, inventory: 40 },
+     ];
+
+     console.log(products)
     //Step 3 Write a for...of loop
+     for(let p of products) {
+        let discount =0;
+        switch (products.category) {
+            case "Skate": discount = .25; break;
+            case "Headwear": discount = .10; break;
+            case "Equipment": discount = .15; break;
+            case "Padding": discount = .30; break;
+            case "Miscellaneous": discount = .05; break;
+            default: discount = 0; break;}
+            products.promoPrice = (p.price * (1 - discount)).toFixed(2);
+        }
+        console.log (products.promoPrice)
+    //Step 4 Create a Variable
